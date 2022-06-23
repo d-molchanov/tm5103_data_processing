56e7af6148ee740d374ce344a7d2e1348f32050d
Dmitry Molchanov <d.a.molchanoff@gmail.com>
2022-06-23 18:02:41 +0300

    Flags added for mode choosing

--- tm5103_data_processing.py
+++ tm5103_data_processing.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from argparse import ArgumentParser
+
+
+def create_parser():
+    parser = ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-s', '--split', action='store_true')
+    group.add_argument('-a', '--average', action='store_true')
+    group.add_argument('-g', '--graph', action='store_true')
+    parser.add_argument('filename', nargs='?')
+
+    return parser
+
 
 if __name__ == '__main__':
     print('This is tm5103 data processing!')
+    argparser = create_parser()
+    args = argparser.parse_args()
+    print(args)
