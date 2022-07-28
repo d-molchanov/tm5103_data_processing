1c9a164212d8dcce2d255e831a2e27c44ce9b6e3
Dmitry Molchanov <d.a.molchanoff@gmail.com>
2022-07-28 12:32:14 +0300

    Created directory for classes and settings

--- tm5103_data_processing.py
+++ tm5103_data_processing.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from argparse import ArgumentParser
-from tm5103_data_parser import TM5103DataParser
+from sources.tm5103_data_parser import TM5103DataParser
 
 
 def create_parser():
